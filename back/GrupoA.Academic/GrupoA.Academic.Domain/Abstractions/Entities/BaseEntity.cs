5af6822fabfcc526ae142acbb8e480321907f122
Leandro Luiz G. Cavalheiro <leandro.cavalheiro@codengage.com>
2022-04-24 09:07:05 -0300

    [Task-2] Criação das Abstração do Domínio. - Criação da BaseEntity - Criação da Interface de Repositório - Em commons: Criação  da extension SetKind para Datetime. A ideia é solucionar uma incompatibilidade do datas do NpgSql6. - Em commons: Criação  da Método Now(). A ideia é padronizar o uso da Date e  Hora atual, caso o SetKind não for a malhor opção de solução  da incompatibilidade de datas, é só corrigir o método Now.

---
+++ back/GrupoA.Academic/GrupoA.Academic.Domain/Abstractions/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using GrupoA.Academic.Commom.Generals;
+namespace GrupoA.Academic.Domain.Abstractions.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public bool IsActive()
+    {
+        return DeletedAt.Equals(null);
+    }
+    public BaseEntity(Guid id)
+    {
+        Id = id;
+        CreatedAt = AcademicMethods.Now();
+        UpdatedAt = CreatedAt;
+    }
+    public BaseEntity()
+    {
+
+    }
+}
---
+++ back/GrupoA.Academic/GrupoA.Academic.Domain/Abstractions/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace GrupoA.Academic.Domain.Abstractions.Interfaces;
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity> GetById(Guid id, bool asNoTracking = false);
+    IQueryable<TEntity> GetAll();
+    Task Add(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity, bool destroy = false);
+    Task<int> SaveChanges();
+    Task<bool> Exists(Expression<Func<TEntity, bool>> predicate);
+}
